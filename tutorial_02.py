50f5cf9317ebb3be261b11c11bbcaf241fef6de9
ryanleek <30751720+ryanleek@users.noreply.github.com>
2020-08-20 22:44:14 +0900

    Create tutorial_02.py

---
+++ tutorial_02.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Welcome to Jupyter!
+
+# In[ ]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+DATADIR - "C:/Datasets/PetImages"
+CATAGORIES = ["DOG", "CAT"]
+
+for category in CATEGORIES:
+    path = os.path.join(DATADIR, category) #path to cats or dogs directory
+    for img in os.listdir(path):
+        #convert images in to an array while grayscaling the image
+        img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img_array, cmap="gray")
+        plt.show()
+
+
+# In[ ]:
+
+
+print(img_array.shape)
+
+
+# In[ ]:
+
+
+IMG_SIZE = 50
+
+new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+
+
+# In[ ]:
+
+
+training_data = []
+
+def create_training_data():
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category) #path to cats or dogs directory
+        class_num = CATEGORIES.index(category)
+        for img in os.listdir(path):
+            try:
+                #convert images in to an array while grayscaling the image
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+                #resize the image
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                #add the image to traing dataset
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+            
+create_training_data()
+
+
+# In[ ]:
+
+
+print(len(training_data))
+
+
+# In[ ]:
+
+
+import random
+#shuffle traing dataset for efficient learning
+random.shuffle(training_data)
+
+
+# In[ ]:
+
+
+X = []
+y = []
+
+for features, label in training_data:
+    X.append(features)
+    y.append(label)
+    
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1) #1 because its grayscale
+
+
+# In[ ]:
+
+
+import pickle
+
+pickle_out = open("X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+
+
+# In[ ]:
+
+
+pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+
+# This repo contains an introduction to [Jupyter](https://jupyter.org) and [IPython](https://ipython.org).
+# 
+# Outline of some basics:
+# 
+# * [Notebook Basics](../examples/Notebook/Notebook%20Basics.ipynb)
+# * [IPython - beyond plain python](../examples/IPython%20Kernel/Beyond%20Plain%20Python.ipynb)
+# * [Markdown Cells](../examples/Notebook/Working%20With%20Markdown%20Cells.ipynb)
+# * [Rich Display System](../examples/IPython%20Kernel/Rich%20Output.ipynb)
+# * [Custom Display logic](../examples/IPython%20Kernel/Custom%20Display%20Logic.ipynb)
+# * [Running a Secure Public Notebook Server](../examples/Notebook/Running%20the%20Notebook%20Server.ipynb#Securing-the-notebook-server)
+# * [How Jupyter works](../examples/Notebook/Multiple%20Languages%2C%20Frontends.ipynb) to run code in different languages.
+
+# You can also get this tutorial and run it on your laptop:
+# 
+#     git clone https://github.com/ipython/ipython-in-depth
+# 
+# Install IPython and Jupyter:
+# 
+# with [conda](https://www.anaconda.com/download):
+# 
+#     conda install ipython jupyter
+# 
+# with pip:
+# 
+#     # first, always upgrade pip!
+#     pip install --upgrade pip
+#     pip install --upgrade ipython jupyter
+# 
+# Start the notebook in the tutorial directory:
+# 
+#     cd ipython-in-depth
+#     jupyter notebook
