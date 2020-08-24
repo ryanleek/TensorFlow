22a2f07063e4342f69b8b9af86648eaf6b0854b6
ryanleek <30751720+ryanleek@users.noreply.github.com>
2020-08-24 21:26:48 +0900

    Update tutorial_03.py

--- tutorial_03.py
+++ tutorial_03.py
@@ -1,11 +1,22 @@
-
-# In[1]:
 import os
-import cv2  #'pip install opencv-python' require
+import time
+import cv2  #'pip install opencv-python' required
+
+import random
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+
+NAME = "CatvDog_Cnn_64x2{}".format(int(time.time()))
+tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
 
 DATADIR = "C:/Datasets/PetImages"
 CATEGORIES = ["DOG", "CAT"]
 IMG_SIZE = 50
+
 training_data = []
 X, y = [], []
 
@@ -26,10 +37,6 @@ def create_training_data():
                 pass
 
 
-# In[28]:
-import random
-import numpy as np
-
 #create training data
 create_training_data()
 
@@ -46,28 +53,28 @@ X = X/255.0 #scale the data to 0~1(max pixel data: 255)
 y = np.array(y)
 
 
-# In[47]:
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+#helpful for running multiple models on one gpu
+#gpu_options = tf.GPUOptions(per_process_gpu_memorty_fraction=0.33)
+#sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 #create sequential model
 model = Sequential()
 
-#input layer: Convolutional layer(64 neurons, 3x3 window, ignore -1, ReLU)
+#input layer: Convolution layer(64 neurons, 3x3 window, ignore -1, ReLU)
 model.add(Conv2D(64, (3,3), input_shape=X.shape[1:]))
 model.add(Activation("relu"))
 #could be regarded as separate layer
 model.add(MaxPooling2D(pool_size=(2,2)))#picks most significant data in pool from conv layer output
 
-#hidden layer 1: Convolutional layer(64 neurons, 3x3 window, ReLU)
+#hidden layer 1: Convolution layer(64 neurons, 3x3 window, ReLU)
 model.add(Conv2D(64, (3,3)))
 model.add(Activation("relu"))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 #hidden layer 2: Flatten-Dense layer(64 neurons)
-model.add(Flatten())    #convert data(input for Dense) to 1D array
+model.add(Flatten())    #convert data(input for Dense == 3D feature map) to 1D array
 model.add(Dense(64))    #
+model.add(Activation("relu"))
 
 #output layer: Dense layer(1 neurons, Sigmoid)
 model.add(Dense(1)) 
@@ -78,5 +85,4 @@ model.compile(loss="binary_crossentropy", #because its either cats or dogs
             optimizer="adam",
             metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
-
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks=[tensorboard])
