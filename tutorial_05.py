9c0ec9c6d2008ff883704d926966340302c7b425
ryanleek <30751720+ryanleek@users.noreply.github.com>
2020-08-27 22:41:00 +0900

    Create tutorial_05.py

---
+++ tutorial_05.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM #or CuDNNLSTM for gpu
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train/255.0
+x_test = x_test
+
+model = Sequential()
+
+model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(128, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.2))
+
+model.add(Dense(10, activation='softmax'))
+
+opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-3)
+
+model.compile(loss='sparse_categorical_crossentropy',
+            optimizer=opt,
+            metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=3, validation_data=(x_test, y_test))
